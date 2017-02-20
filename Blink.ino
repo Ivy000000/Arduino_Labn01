7d401706c9953dc93fc0b70efb4fa92b27fa833d
Ivy000000 <ivy9297@gmail.com>
2017-02-20 18:59:44 +0800

    1000->2000(ms) and pin 13->9

--- Blink.ino
+++ Blink.ino
@@ -22,8 +22,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(2000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
   delay(2000);              // wait for a second
 }
